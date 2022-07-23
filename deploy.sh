cebbe2e55e83e30a435a000ef808aadf0ab154ec
Pierce <pierce.tickle@gmail.com>
2022-07-22 23:20:56 -0400

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ rm -rf .parcel-cache
 
 yarn build
 
-git subtree split --prefix dist -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git subtree split --prefix dist -b gh-pages
 git checkout gh-pages
 git add .
 git commit -m "Deploy"
-git push
+git push -f
 git checkout main
-git branch -D gh-pages # delete the local gh-pages because you will need it: ref
+git branch -D gh-pages
